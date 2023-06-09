4e529632c46127b2cce4ccc15313a92954a2adc7
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-06-09 14:17:22 -0300

    Finalizando Módulo 7- Relacionamento N-N

---
+++ src/main/java/br/com/treinaweb/twprojetos/entidades/Funcionario.java
@@ -0,0 +1,59 @@
+package br.com.treinaweb.twprojetos.entidades;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+@Entity
+public class Funcionario extends Pessoa {
+
+
+    @Column(name = "data_admissao", nullable = false)
+    @DateTimeFormat(iso = ISO.DATE)
+    private LocalDate dataAdmissao;
+
+    @Column(name = "data_demissao")
+    @DateTimeFormat(iso = ISO.DATE)
+    private LocalDate dataDemissao;
+
+    
+    @ManyToOne
+    @JoinColumn(name = "cargo_id_fk", nullable = false)
+	private Cargo cargo;
+
+
+    public LocalDate getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(LocalDate dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public LocalDate getDataDemissao() {
+        return dataDemissao;
+    }
+
+    public void setDataDemissao(LocalDate dataDemissao) {
+        this.dataDemissao = dataDemissao;
+    }
+
+	public Cargo getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(Cargo cargo) {
+		this.cargo = cargo;
+	}
+
+
+
+	
+	
+}
